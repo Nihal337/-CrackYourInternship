e6d0f33dbf44a5108aa5e62cb8131834461f3aa6
Nihal Tiwari <83083841+Nihal337@users.noreply.github.com>
2022-05-03 16:03:06 +0530

    Create sort-colors.cpp

---
+++ array/sort-colors.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    // 2nd approach : using DNF : dutch national....
+    // or u can say using three pointers 
+    void sortColors(vector<int>& nums) {
+        
+        int n = nums.size() ;
+        if(n==1) return ;
+        int left = 0 ,mid = 0, right = n-1 ;
+          
+        while(mid<=right) {
+            
+            if(nums[mid]==0) {
+                swap(nums[mid],nums[left])  ;
+                left++ ;
+                mid++ ;
+            }
+            else if(nums[mid]==2) {
+                swap(nums[mid],nums[right]) ;
+                right-- ;
+               
+            }
+            else mid++ ;
+        }
+    }
+};
